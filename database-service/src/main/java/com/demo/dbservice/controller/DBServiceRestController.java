871398ad286729ae740ac6b60cf8190eab2e78ba
Bhaskar Pal <69039695+pal-bhaskar@users.noreply.github.com>
2021-12-08 21:10:17 +0530

    Update DBServiceRestController.java
    Updated logger

--- database-service/src/main/java/com/demo/dbservice/controller/DBServiceRestController.java
+++ database-service/src/main/java/com/demo/dbservice/controller/DBServiceRestController.java
@@ -3,6 +3,8 @@ package com.demo.dbservice.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,8 @@ import com.demo.dbservice.model.Customer;
 @RequestMapping("/private/v1")
 public class DBServiceRestController {
 	
+	private Logger log = LoggerFactory.getLogger(DBServiceRestController.class);
+	
 	@Autowired
 	private DatabaseService databaseService;
 
@@ -25,12 +29,15 @@ public class DBServiceRestController {
 	public List<Customer> getAllCustomer() {
 		List<Customer> customerList = new ArrayList<>();
 		databaseService.findAll().forEach(customerList :: add);
+		log.info("returning customer list ... {}",customerList);
 		return customerList;
 	}
 	
 	@PostMapping("/add/customer")
 	public void addContent(@RequestBody Customer customer) {
+		log.info("adding customer ... {}",customer);
 		databaseService.save(customer);
+		log.info("successfully added new customer...");
 	}
 	
 }
